3211bde5d4509078f7a02638d44cb0d2524e93ca
PolinaRyu <80034164+PolinaRyu@users.noreply.github.com>
2023-04-13 21:40:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 503882767 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    if stats.kstest(y,x).pvalue < 0.06:
+        flag = True
+    else:
+        flag = False
+    
+    return flag
